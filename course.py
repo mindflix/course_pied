e469eb089c83789ce483a8fc02b08d813a8b2667
mindflix <nyco.demol@gmail.com>
2021-04-21 02:39:17 +0200

    fix id

--- course.py
+++ course.py
@@ -9,13 +9,18 @@ output = {"data": []}
 class User:
     def __init__(self, user):
         self.user = user
+        self.is_json_empty()
+
+    def is_json_empty(self):
+        with open("./data/timetable.json", "r+") as file:
+            try:
+                json_string = json.load(file)
+            except (json.JSONDecodeError):
+                json.dump(output, file)
 
     def add_time_slot(self, start, end):
         time_dict = {}
-        if self.get_last_id() == None:
-            time_dict["id"] = 1
-        else:
-            time_dict["id"] = self.get_last_id() + 1
+        time_dict["id"] = self.get_last_id() + 1
         time_dict["user"] = self.user
         time_dict["duration"] = [start, end]
         output["data"].append(time_dict)
@@ -25,10 +30,23 @@ class User:
             json_string = json.load(file)
             json_data = json_string["data"]
             try:
-                last_id = json_data[-1]["id"]
-                return last_id
+                last_id_json = json_data[-1]["id"]
             except (IndexError, KeyError):
-                return None
+                last_id_json = 0
+
+            try:
+                last_id_output = output["data"][-1]["id"]
+            except (IndexError, KeyError):
+                last_id_output = 0
+
+            if last_id_json == 0 and last_id_output == 0:
+                return 0
+            elif last_id_json > 0 and last_id_output == 0:
+                return last_id_json
+            elif last_id_json == 0 and last_id_output > 0:
+                return last_id_output
+            else:
+                return last_id_output
 
     def write_json(self):
         with open("./data/timetable.json", "r+") as file:
